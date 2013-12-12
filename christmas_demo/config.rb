b9b60bacc6c1f96df396b60b8d46cdd051c7f064
Robert White <me@rob.pw>
2013-12-12 14:23:50 +0000

    Christmas demo, initial commit

--- christmas_demo/config.rb
+++ christmas_demo/config.rb
@@ -1,6 +1,10 @@
 # Require any additional compass plugins here.
 add_import_path "bower_components/foundation/scss"
 
+require 'animation'
+require 'sassy-math'
+
+
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
